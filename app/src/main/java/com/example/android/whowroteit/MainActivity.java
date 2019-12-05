170c062ef8eb4e4f1dabe6fd4d583ecb3569e9ed
BvSiT <bvstraten1@gmail.com>
2019-12-05 14:10:39 +0100

    Solution using AsyncTaskLoader

--- app/src/main/java/com/example/android/whowroteit/MainActivity.java
+++ app/src/main/java/com/example/android/whowroteit/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.android.whowroteit;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,7 +14,10 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<String> {
     private EditText mBookInput;
     private TextView mTitleText;
     private TextView mAuthorText;
@@ -43,6 +50,11 @@ public class MainActivity extends AppCompatActivity {
 
         if (networkInfo != null && networkInfo.isConnected()
                 && queryString.length() != 0) {
+
+            Bundle queryBundle = new Bundle();
+            queryBundle.putString("queryString",queryString);
+            getSupportLoaderManager().restartLoader(0,queryBundle,this);
+
             new FetchBook(mTitleText, mAuthorText).execute(queryString);
             mAuthorText.setText("");
             mTitleText.setText(R.string.loading);
@@ -57,6 +69,67 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
+
+    }
+
+    @NonNull
+    @Override
+    public Loader<String> onCreateLoader(int i, @Nullable Bundle args) {
+        String queryString = "";
+        if (args != null) {
+            queryString = args.getString("queryString");
+        }
+
+        return new BookLoader(this, queryString);
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<String> loader, String s) {
+        try{
+            JSONObject jsonObject = new JSONObject(s);
+            JSONArray jsonArray = jsonObject.getJSONArray("items");
+            int i = 0;
+            String title = null;
+            String authors = null;
+
+            while (i<jsonArray.length()&& (title==null && authors==null)){
+                JSONObject book = jsonArray.getJSONObject(i);
+                JSONObject volumeInfo = book.getJSONObject("volumeInfo");
+
+                // Try to get the author and title from the current item,
+                // catch if either field is empty and move on.
+                try {
+                    title = volumeInfo.getString("title");
+                    JSONArray authors_array = volumeInfo.getJSONArray("authors");
+                    StringBuilder builderAuthors = new StringBuilder();
+                    for (int j=0;j< authors_array.length();j++){
+                        if (j>0) builderAuthors.append(", ");
+                        builderAuthors.append(authors_array.get(j).toString());
+                    }
+                    authors = builderAuthors.toString();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                i++;
+            }
+
+            if (title!=null && authors!=null){
+                mAuthorText.setText(authors);
+                mTitleText.setText(title);
+            }
+            else {
+                mAuthorText.setText(R.string.no_results);
+                mTitleText.setText("");
+            }
+        }
+        catch (Exception e){
+            mAuthorText.setText(R.string.no_results);
+            mTitleText.setText("");
+        }
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<String> loader) {
 
     }
 }
